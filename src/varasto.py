fe4f58a78e9f4b1ca6b784d7786ecf43bb7ca52f
meri saynatkari <meri.saynatkari@gmail.com>
2021-11-14 20:37:42 +0200

    wk2 ex4 pt2

--- src/varasto.py
+++ src/varasto.py
@@ -1,13 +1,13 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
         # siis tämähän on täysin sama asia lyhemmin ilmaistuna mutta nyt menee pylintistä läpi??
-        # self.tilavuus = tilavuus if tilavuus > 0.0 else 0.0
+        self.tilavuus = tilavuus if tilavuus > 0.0 else 0.0
 
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+        # if tilavuus > 0.0:
+        #     self.tilavuus = tilavuus
+        # else:
+        #     # virheellinen, nollataan
+        #     self.tilavuus = 0.0
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
