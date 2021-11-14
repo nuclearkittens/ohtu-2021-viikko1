d6cacb5ea394ae6612e6c6b65ca727600f27cb7c
meri saynatkari <meri.saynatkari@gmail.com>
2021-11-14 20:28:08 +0200

    wk2 ex3

--- src/varasto.py
+++ src/varasto.py
@@ -1,11 +1,14 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-        
+        # siis tämähän on täysin sama asia lyhemmin ilmaistuna mutta nyt menee pylintistä läpi??
+        self.tilavuus = tilavuus if tilavuus > 0.0 else 0.0
+
+        # if tilavuus > 0.0:
+        #     self.tilavuus = tilavuus
+        # else:
+        #     # virheellinen, nollataan
+        #     self.tilavuus = 0.0
+
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -31,7 +34,7 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +43,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
